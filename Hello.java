f80ad5f7dc170bd29804eaa02b9dc8c7c0fe300d
learn-madhu <madhusmita186@gmail.com>
2018-10-08 23:26:42 +0530

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("hello, testing my java class from Jenkins.");
+		System.out.println("hello, testing my java change.");
 	}
 }
